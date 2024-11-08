651087552f67890c89fe2e79e78831199deffd97
narestakian <n.arestakian@gmail.com>
2024-11-08 19:28:13 +0400

    task_1_Nino

---
+++ src/test/java/AnnotationsAndStreamApi1.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+public class AnnotationsAndStreamApi1 {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Alice", "Bob", "Eve", " Charlie", "Daniel");
+        Stream<String> stream = names.stream();
+        List <String> listFiltered = stream.filter(item -> item.contains("e"))
+                .collect(Collectors.toList());
+        System.out.println(listFiltered);
+    }
+}
+
